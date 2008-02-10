9a554b05fc9f058afb8267b24361f312f1dba2bf
kovrov <kovrov@gmail.com>
2008-02-10 22:36:15 +0000

    particles demo: more of camera rotation.

--- particles/camera.py
+++ particles/camera.py
@@ -1,29 +1,39 @@
 from pyglet.gl import *
-from utils import normalizeVector, addVectors
+from utils import normalizeVector, addVectors, rotateVectorY
+import math
 
-position  = [0.0, 0.0, 0.0]
-direction = [0.0, 0.0, 0.0]
+position  = (0.0, 0.0,  0.0)
+direction = (0.0, 0.0, -1.0)
+look_at   = (0.0, 0.0, -1.0)
 
 def draw_task():
 	while True:
 		yield
 		glLoadIdentity()
+		gluLookAt(position[0], position[1], position[2],  # eye
+		          look_at[0], look_at[1], look_at[2],  # center
+		          0.0, 1.0, 0.0) # up
 		#glRotatef(-RotatedX , 1.0, 0.0, 0.0);
 		#glRotatef(-RotatedY , 0.0, 1.0, 0.0);
 		#glRotatef(-RotatedZ , 0.0, 0.0, 1.0);
-		glTranslatef(-position[0], -position[1], -position[2])
+		#glTranslatef(-position[0], -position[1], -position[2])
 
 def rotate_horizontal(degrees):  # left/right
-	direction = rotateVector(direction, degrees)
+	global direction, look_at
+	direction = rotateVectorY(direction, math.radians(-degrees))
+	look_at = addVectors(position, direction)
 
 def rotate_vertical(degrees):  # up/down
 	pass
 
 def move(amount, vect):
-	direction[0] + vect[0]
-	direction[1] + vect[1]
-	direction[2] + vect[2]
-	move_vect = normalizeVector(addVectors(direction, vect))
-	position[0]  += move_vect[0] * amount
-	position[1]  += move_vect[1] * amount
-	position[2]  += move_vect[2] * amount
+	global position, look_at
+	# move vector's angle
+	dot = sum(x * y for x, y in zip((0.0, 0.0, -1.0), vect))  # dot product
+	move_angle = math.acos(dot)
+	# translation vector is final direction of movement
+	translation_vect = rotateVectorY(direction, move_angle)
+	position = (position[0] + translation_vect[0] * amount,
+	            position[1] + translation_vect[1] * amount,
+	            position[2] + translation_vect[2] * amount)
+	look_at = addVectors(position, direction)
--- particles/utils.py
+++ particles/utils.py
@@ -10,8 +10,22 @@ def normalizeVector(v):
 	k = 1.0 / math.sqrt(v[0]**2.0 + v[1]**2.0 + v[2]**2.0)
 	return (v[0] * k, v[1] * k, v[2] * k)
 
-def rotateVector(vect, angle):
-	angle = math.radians(angle)
-	z = vect[2] * math.cos(angle) - vect[0] * math.sin(angle)
+def rotateVectorY(vect, angle):
+	if angle == 0.0:
+		return vect
+	if angle == 180.0:
+		return (-vect[0], vect[1], -vect[2])
 	x = vect[2] * math.sin(angle) + vect[0] * math.cos(angle)
-	return(x, vect[1], z)
+	z = vect[2] * math.cos(angle) - vect[0] * math.sin(angle)
+	return (x, vect[1], z)
+
+if __name__ == "__main__":
+	vect = (0.0, 0.0, 1.0)
+	print "vect", vect
+	print " 45", rotateVectorY(vect, 45.0)
+	print " 90", rotateVectorY(vect, 90.0)
+	print "135", rotateVectorY(vect, 135.0)
+	print "225", rotateVectorY(vect, 225.0)
+	print "270", rotateVectorY(vect, 270.0)
+	print "315", rotateVectorY(vect, 315.0)
+
