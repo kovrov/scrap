7d7ea231d7b201996ad6fa095eb32bc251b479e2
kovrov <kovrov@gmail.com>
2009-09-09 22:34:12 +0000

    Empty Message

--- mahjong.py
+++ mahjong.py
@@ -8,6 +8,12 @@ class Slot:
 	def __init__(self, pos):
 		self.pos = pos
 		self.tile = None
+	def is_open(self):
+		if any(s.tile for s in self.above_blockers):
+			return False
+		if any(s.tile for s in self.left_blockers) and any(s.tile for s in self.right_blockers):
+			return False
+		return True
 
 def blockify(slots):
 	for slot in slots:
@@ -44,28 +50,77 @@ board = layouts['dragon']
 pyglet.resource.path.append('res')
 pyglet.resource.reindex()
 background_board = pyglet.resource.image('Background_board.jpg')
-tiles_normal = pyglet.resource.image('tiles_normal.png')
-tiles_normal_seq = pyglet.image.ImageGrid(tiles_normal, 6, 7)
-tiles_tex_grid = pyglet.image.TextureGrid(tiles_normal_seq)
+tiles_image = pyglet.resource.image('tiles.png')
+tiles_image_seq = pyglet.image.ImageGrid(tiles_image, 6, 7)
+tex_grid = pyglet.image.TextureGrid(tiles_image_seq)
 off = 8  # "height offset" value - hardcoded into bitmap
 
-#       ball tiles,             bamboo tiles,            characters tiles,         seasons tiles,          wind tiles,               flowers tiles,          dragons tiles
-tiles = tiles_tex_grid[0:9]*4 + tiles_tex_grid[9:18]*4 + tiles_tex_grid[18:27]*4 + tiles_tex_grid[27:31] + tiles_tex_grid[31:35]*4 + tiles_tex_grid[35:39] + tiles_tex_grid[39:42]*4
-random.shuffle(tiles)
-for i in range(len(board)):
-	board[i].tile = tiles[i]
+class Tile:
+	def __init__(self, texture, match):
+		self.texture = texture
+		self.match = match
 
-window = pyglet.window.Window((tiles_tex_grid.item_width-off) * 16, (tiles_tex_grid.item_height-off) * 9)  # 16x9 tiles
+tiles = []
+# each of 9 "ball" tiles have 4 identical matches
+tiles += [Tile(t,m) for t,m in zip(tex_grid[0:9], range(9))]*4
+# each of 9 "bamboo" tiles have 4 identical matches
+tiles += [Tile(t,m) for t,m in zip(tex_grid[9:18], range(9,18))]*4
+# each of 9 "character" tiles have 4 identical matches
+tiles += [Tile(t,m) for t,m in zip(tex_grid[18:27], range(18,27))]*4
+# all of 4 "season" tiles match one another
+tiles += [Tile(texture,27) for texture in tex_grid[27:31]]
+# each of 4 "wind" tiles have 4 identical matches
+tiles += [Tile(t,m) for t,m in zip(tex_grid[31:35], range(28,32))]*4
+# all of 4 "flower" tiles match one another
+tiles += [Tile(texture,32) for texture in tex_grid[35:39]]
+# each of 3 "dragon" tiles have 4 identical matches
+tiles += [Tile(t,m) for t,m in zip(tex_grid[39:42], range(33,36))]*4
+
+random.shuffle(tiles)
+for slot, tile in zip(board, tiles):
+	slot.tile = tile
+
+selected = None
+
+window = pyglet.window.Window((tex_grid.item_width-off) * 16, (tex_grid.item_height-off) * 9)  # 16x9 tiles
 @window.event
 def on_draw():
 	background_board.blit(0, 0)
-	for i in board:
-		if i.tile is None:
+	for slot in board:
+		if slot.tile is None:
 			continue
-		x,y,z = i.pos
-		real_x = x * (tiles_tex_grid.item_width-8)/2  + z * 8
-		real_y = y * (tiles_tex_grid.item_height-8)/2 + z * 8
-		i.tile.blit(real_x, real_y)
+		slot_x, slot_y, slot_z = slot.pos
+		tile_x = slot_x * (tex_grid.item_width - off) / 2 + slot_z * off
+		tile_y = slot_y * (tex_grid.item_height - off) / 2 + slot_z * off
+		if selected is slot:
+			glColor3f(1,0.5,0.5)
+		else:
+			glColor3f(1,1,1)
+		slot.tile.texture.blit(tile_x, tile_y)
+@window.event
+def on_mouse_press(x, y, button, modifiers):
+	global selected
+	tile_width = tex_grid.item_width - off
+	tile_height = tex_grid.item_height - off
+	for slot in reversed(board):
+		if slot.tile is None:
+			continue
+		slot_x, slot_y, slot_z = slot.pos
+		tile_x = slot_x * tile_width / 2 + (slot_z + 1) * off
+		tile_y = slot_y * tile_height / 2 + (slot_z + 1) * off
+		if tile_x <= x and x < tile_x + tile_width and tile_y <= y and y < tile_y + tile_height:
+			if not slot.is_open():
+				return
+			if selected is None:
+				selected = slot
+			elif selected is slot:
+				selected = None
+			elif selected.tile.match == slot.tile.match:
+				selected.tile = None
+				slot.tile = None
+				selected = None
+			return
+
 glEnable(GL_BLEND)
 glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 pyglet.app.run()
